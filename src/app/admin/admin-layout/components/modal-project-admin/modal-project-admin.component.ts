404faacbc8d7e3195625f523ad204a4c87aee12f
Anibal <mancillaanibal99@gmail.com>
2025-10-21 23:24:21 -0500

    seccion proyectos, editar

--- src/app/admin/admin-layout/components/modal-project-admin/modal-project-admin.component.ts
+++ src/app/admin/admin-layout/components/modal-project-admin/modal-project-admin.component.ts
@@ -1,9 +1,10 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Project } from '../../../interface/project.interface';
-import { ReactiveFormsModule, ɵInternalFormsSharedModule } from "@angular/forms";
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from "@angular/forms";
 import { CommonModule, DatePipe } from '@angular/common';
 import { SupabaseService } from '../../../../services/supabase/supabase.service';
+import { ProyectoService } from '../../../../services/proyectos/proyecto.service';
 
 export interface ModalProjectDataAdmin {
     project: Project;
@@ -28,48 +29,118 @@ interface Categoria {
 
 export class ModalProjectAdminComponent {
     private supabase = inject( SupabaseService ).supabaseClient;
+    private proyectService = inject( ProyectoService );
     dialogRef = inject( MatDialogRef<ModalProjectAdminComponent> );
     data = inject<any>( MAT_DIALOG_DATA );
     openCategorias = signal<boolean>( false );
+    loading = signal<boolean>( false );
 
     categoriaSeleccionada = signal<Categoria | null>( null );
     miFecha = new Date( this.data.project.fecha );
     categorias = signal<any>( [] );
+    fb = inject( FormBuilder );
+
+    proyectoForm = this.fb.group( {
+        titulo: [ '', Validators.required ],
+        idcategoria: [ 0 ],
+        fecha: [ new Date(), Validators.required ],
+        // cliente: [ '' ],
+        ubicacion: [ '', Validators.required ],
+        descripcioncorta: [ '', Validators.required ],
+        descripcioncompleta: [ '',  Validators.required ],
+        imagenprincipal: [ '', Validators.required ],
+    } );
 
 
-    ngOnInit(): void {
-        // console.log( this.data.project );
-        // console.log( this.data );
-        // this.categories = await this.getCategorias();
-        // this.categorias.set( this.getCategorias() );
-        // effect( () => {
-        //     console.log(this.categorias())
-        // })
 
-        this.getCategoriasHarcode()
-    
-        
+    async onSubmit() {
+        const isValid = this.proyectoForm.valid;
+        // console.log( this.proyectoForm.value, { isValid } );
+
+        this.proyectoForm.markAllAsTouched();
+        this.loading.set( true );
+
+        if ( !isValid ) return;
+
+
+        try {
+            const proyectoActualizado = { ...this.proyectoForm.value, idcategoria: this.proyectoForm.value.idcategoria};
+            const id = this.data.project.id;
+
+            const response = await this.proyectService.updateProyecto( id, proyectoActualizado );
+            console.log( "Proyecto actualizado", response );
+            console.log( "El id: ", id );
+            this.closeModal();
+            // const productLike: Partial<any> = {
+            //     ...(formValue),
+            // }
+        } catch ( error ) {
+            console.log( error );
+            console.log( "Error al actualizar productos, desde modal-project-admin" );
+        } 
     }
 
+    categoriaSeleccionadaLectura = computed( () => {
+        // busca la categoría cuyo 'seleccionado' sea true
+
+        return this.categorias().find( ( c: any ) => c.seleccionado );
+    } );
+
+
+    async ngOnInit() {
+        
+        if ( this.data?.project ) {
+            const formValue = this.data.project;
+            this.proyectoForm.patchValue( {
+                titulo: formValue.titulo,
+                idcategoria: formValue.idcategoria,
+                fecha: formValue.fecha,
+                // cliente: '',
+                ubicacion: formValue.ubicacion,
+                descripcioncorta: formValue.descripcioncorta,
+                descripcioncompleta: formValue.descripcioncompleta,
+                imagenprincipal: formValue.imagenprincipal,
+            } );
+        }
+        await this.getCategorias();
+        await this.getCategoriasHarcode();
+        console.log( this.data.project.idcategoria );
+
+
+    }
+    //! COMENTAR POR AHORA
     async getCategoriasHarcode() {
-        const { data, error } = await this.getCategorias()
-        this.categorias.set(data?.map( dataAddSeleccionado => {
+        const { data, error } = await this.getCategorias();
+        this.categorias.set( data?.map( dataAddSeleccionado => {
+
+            if ( dataAddSeleccionado.id === this.data?.project.idcategoria ) {
+                return {
+                    ...dataAddSeleccionado,
+                    seleccionado: true,
+                };
+            }
+
             return {
                 ...dataAddSeleccionado,
                 seleccionado: false,
-            }
-        }))
+            };
+        } ) );
 
-        console.log(this.categorias())
+        console.log( 'categoriasHarcode', this.categorias() );
 
     }
+
+    //! COMENTAR POR AHORA
+
+
+
 
     async getCategorias() {
         let { data, error } = await this.supabase
             .from( 't_categoria' )
             .select( 'id, nombre' );
         // return t_categoria;
-        return { data, error }
+        return { data, error };
     }
 
     seleccionarCategoria( categoria: Categoria ) {
@@ -79,7 +150,23 @@ export class ModalProjectAdminComponent {
             list.map( ( c: any ) => ( { ...c, seleccionado: c.id === categoria.id } ) )
         );
 
-        console.log(this.categoriaSeleccionada())
+        this.proyectoForm.patchValue({
+            idcategoria: categoria.id
+        })
+
+        console.log( 'categoria seleccionado', this.categoriaSeleccionada() );
+    }
+
+    isValidField( form: FormGroup, fieldName: string ) {
+        return (
+            !!form.controls[ fieldName ]?.errors && form.controls[ fieldName ]?.touched
+        );
+    }
+
+    isValidFieldInArray( formArray: FormArray, index: number ) {
+        return (
+            formArray.controls[ index ].errors && formArray.controls[ index ].touched
+        );
     }
 
     closeModal() {
--- src/app/admin/admin-layout/proyectos/proyectos.component.ts
+++ src/app/admin/admin-layout/proyectos/proyectos.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { SupabaseService } from '../../../services/supabase/supabase.service';
 import { Project } from '../../interface/project.interface';
 import { ModalProjectAdminComponent } from '../components/modal-project-admin/modal-project-admin.component';
+import { ProyectoService } from '../../../services/proyectos/proyecto.service';
 
 
 @Component( {
@@ -16,79 +17,32 @@ import { ModalProjectAdminComponent } from '../components/modal-project-admin/mo
 export class ProyectosComponent {
 
 
-    private supabase = inject( SupabaseService ).supabaseClient;
+    // private supabase = inject( SupabaseService ).supabaseClient;
     // form: FormBuilder = inject( FormBuilder );
-
-    projects: Project[] = [];
-    // projectWithCategories : Pick<Project, { nombre: string }[]>
+    private projectService = inject( ProyectoService );
+    
     categories: { nombre: string; }[] = [];
     displayedColumns: string[] = [ 'imagenprincipal', 'titulo', 'categoria', 'fecha', 'ubicacion', 'acciones' ];
     // dataSource = this.projects;
 
     dialog = inject( MatDialog );
 
-    // categorias = signal<any>( [
-    //     { id: 1, nombre: 'Residencial', seleccionado: false },
-    //     { id: 2, nombre: 'Comercial', seleccionado: false },
-    //     { id: 3, nombre: 'Cultural', seleccionado: false },
-    //     { id: 4, nombre: 'Público', seleccionado: false },
-    //     { id: 5, nombre: 'Hospitalidad', seleccionado: false },
-    //   ] );
-    // formGrupo = this.form.group( {
-    //     titulo: new FormControl( '', [ Validators.required ] ),
-    //     categoria: new FormControl( '' ),
-    //     fecha: new FormControl(''),
-    //     ubicacion: new FormControl('', [Validators.required]),
-    //     descripcioncorta: new FormControl('', [Validators.required]),
-    //     descripcioncompleta: new FormControl('', [Validators.required]),
-    //     imagenprincipal: new FormControl('', [Validators.required]),
-    //     galeriaimagenes: new FormControl( ''),
-        
-    // } );
-
+    
+    projects = this.projectService.projectSignal;
     async ngOnInit() {
-        this.projects = await this.getProjects() ?? [];
-        this.categories = await this.getCategorias();
-        // this.projectWithCategories = [...await this.getProjects() ?? [], ]
+
+        this.projectService.getProjects();
+        
         console.log( this.projects );
+        
         console.log( this.categories );
-        // this.projects = [...this.projects, ...this.categories];
+      
     }
-
-    async getProjects(): Promise<Project[] | null> {
-        let { data: t_proyecto, error } = await this.supabase.from( 'v_obtenerproyectos' )
-            .select( '*' );
-        return t_proyecto ?? [];
-    }
-
-    async getCategorias(): Promise<{ nombre: string; }[]> {
-        let { data: t_categoria, error } = await this.supabase
-            .from( 't_categoria' )
-            .select( '*' );
-        console.log( t_categoria );
-        return t_categoria ?? [];
-    }
-
-    // seleccionarCategoria( categoria: any ) {
-    //     this.formGrupo.get( 'categoria' )?.setValue( categoria.nombre );
-
-    //     this.categorias.update( ( list ) =>
-    //         list.map( ( c: any ) => ( { ...c, seleccionado: c.id === categoria.id } ) )
-    //     );
-
-    //     console.log( this.categorias() );
-    // }
-
 
     openModalDetalle( project : any ) {
         this.dialog.open( ModalProjectAdminComponent, {
             width: '650px',
             height: '80%',
-            //   data: project,
-            // data: {
-            //     project,
-            //     categories: this.categories
-            // }
             data: { project }
 
         } );
